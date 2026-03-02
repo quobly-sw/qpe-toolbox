fd315a18b7469ad3e3503ee17083f6d3dba48ec7
thibxlv <thibaud.louvet@quobly.io>
2026-03-02 15:23:09 +0100

    Update examples/textbook_qpe.py
    Co-authored-by: Olivier Gauthé <olivier.gauthe@quobly.io>

--- examples/textbook_qpe.py
+++ examples/textbook_qpe.py
@@ -31,7 +31,7 @@
 # 3. The circuit starts with a Hadamard wall to put the phase register into a superposition state.
 # 4. Then we *encode* the phase into the phase register via a sequence of controlled powers of $U$: $U^{2^k}, k=0,1,...,m-1$ is applied to the physical register, conditioned on the $k$-th phase qubit.
 # 5. Finally to *decode* the phase, we apply the inverse Quantum Fourier Transform on the phase register.
-# 6. We measure the phase register and find a $m$-bits approximation to $\theta$ with probability $\propto \Omega$ (at least $\Omega~4/\pi^2$, see below).
+# 6. We measure the phase register and find a $m$-bits approximation to $\theta$ with probability $\propto \Omega$ (at least $4\Omega/\pi^2$, see below).
 # 7. After the measure, the physical register has been projected onto $\ket{u}$.
 #
 # The notebook is organised as follows:
