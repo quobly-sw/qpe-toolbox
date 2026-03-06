4ddcbb6c0916a9b214b807e7d9182d9a23bf4430
Olivier Gauthé <olivier.gauthe@quobly.io>
2026-03-06 10:43:44 +0100

    fix tests

--- tests/test_walk_qpe.py
+++ tests/test_walk_qpe.py
@@ -5,10 +5,10 @@ from qpe_toolbox.hamiltonian import do_dmrg, heisenberg_hamiltonian
 
 
 def test_walk_qpe():
-    n_qubits = 2
+    num_qubits = 2
     m_ph = 2
 
-    H = heisenberg_hamiltonian(n_qubits)
+    H = heisenberg_hamiltonian(num_qubits)
     E0, psi0_mps = do_dmrg(H)
     lmb = sum([abs(P[0]) for P in H.terms])
 
