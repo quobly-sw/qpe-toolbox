b6a2ab11cf3e1daa8cd2fc0913fd90925dbc35ff
Olivier Gauthé <olivier.gauthe@quobly.io>
2026-03-23 18:06:19 +0100

    add main module docstring

--- src/qpe_toolbox/__init__.py
+++ src/qpe_toolbox/__init__.py
@@ -6,6 +6,21 @@
 # project root.
 #
 # --------------------------------------------------------------------------------------
+"""
+QPE Toolbox: A Quantum Phase Estimation Toolbox.
+
+Documentation is available in the doscstrings and online
+at https://quobly-sw.github.io/qpe-toolbox/
+
+Subpackages must be explicitly imported:
+::
+
+- qpe_toolbox.circuit           --- creation and manipulation of `quimb` circuits.
+- qpe_toolbox.estimation        --- perform different flavors of Quantum Phase Estimation.
+- qpe_toolbox.hamiltonian       --- class for defining Hamiltonians and interface with `pyscf` for chemistry.
+- qpe_toolbox.tensor            --- manipulation of Matrix Product Operators (MPO) and Matrix Product States (MPS).
+"""
+
 import enum
 
 __version__ = "1.0.0"
