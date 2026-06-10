c8c2df10282183a10fcf32cb250e770992297c42
Olivier Gauthé <olivier.gauthe@quobly.io>
2026-06-10 17:31:11 +0200

    idiomatic phrasing

--- examples/chemistry_to_qubit.py
+++ examples/chemistry_to_qubit.py
@@ -36,7 +36,7 @@ from qpe_toolbox.hamiltonian import chemistry_hamiltonian
 # %% [markdown]
 # ## $H_2$
 #
-# Let us start with the $H_2$ molecule in the minimal atomic orbital basis set STO-3G. We give the geometry and basis set  as keyword arguments to the function `pyscf.gto.M` to initialize the molecular structure object (spin and charge can also be specified, see below $O_2$).
+# Let us start with the $H_2$ molecule in the minimal atomic orbital basis set STO-3G. We give the geometry and basis set as keyword arguments to the function `pyscf.gto.M` to initialize the molecular structure object (spin and charge can also be specified, see below $O_2$).
 # Then we call the `chemistry_hamiltonian`.
 #
 # * The `hf_mode` parameter describes how spin is assigned to the spatial molecular orbitals in the Hartree-Fock calculations. Restricted Hartree-Fock (`"rhf"`) uses the same molecular orbital twice, one for each state in the spin doublet, while [Unrestricted Hartree-Fock](https://en.wikipedia.org/wiki/Unrestricted_Hartree%E2%80%93Fock) (`"uhf"`) uses different molecular orbitals for the different eigenstates of $S_z$. Unrestricted Hartree-Fock is mostly used for open-shell molecules.
@@ -56,7 +56,7 @@ print(f"n_qubits = {h2_ham_sto3g.n_qubits}")
 #
 # The FCI energy is given by exact diagonalization of the Hamiltonian. It is the best ground state energy estimate within this choice of basis set.
 # In chemistry, an energy estimate must reach the chemical accuracy standard, defined to be $1.6 \text{mHa} \approx 500 \text{K}$.
-# The HF energy is far from the FCI energy by $20 \text{mHa}$, while the CCSD energy is close within $10^{-6} \text{Ha}$.
+# The HF energy differs from the FCI energy by $20 \text{mHa}$, while the CCSD energy is close within $10^{-6} \text{Ha}$.
 #
 # However, the basis set itself only gives an approximate description of the true solutions to the Schrödinger equation. Indeed, note that with this small basis set, the FCI energy is $-1.13$ Ha, while reported values in the literature ([here](https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.83.2541)) for $H_2$ are around $-1.16$ Ha. Even with exact diagonalization, the accuracy is at best $30 \text{mHa}$ in this basis.
 # Hence, reaching chemical accuracy will always mean using much larger basis sets.
@@ -85,7 +85,7 @@ display(h2_mpo)
 # %% [markdown]
 # Here we use $\texttt{quimb}$'s solver directly to introduce the different settings of a DMRG calculation. Note that the `do_dmrg` function of the `hamiltonian` module performs a basic DMRG calculation with default settings.
 #
-# In DMRG, the wavefunction is represented as an MPS. The energy is obtained as `(MPS.H) @ MPO @ MPS` where `@` indicates a contraction and `MPS.H` is the hermitian conjugate. The DMRG algorithm successively optimizes each tensor of the MPS to minimize the energy. Once all the tensors have been optimized in a forward pass, the pass is carried backward: this back-and-forth optimization constitutes a full DMRG sweep. The maximum number of sweeps can be set with `max_sweeps`. Compression is performed along the algorithm, controlled by setting a maximal bond dimension and a cutoff. The `bond_dims` and `cutoffs` arguments take either a single value or a sequence of values (if one wants to assign different parameters to different sweeps).
+# In DMRG, the wavefunction is represented as an MPS. The energy is obtained as `(MPS.H) @ MPO @ MPS` where `@` indicates a contraction and `MPS.H` is the hermitian conjugate. The DMRG algorithm successively optimizes each tensor of the MPS to minimize the energy. Once all the tensors have been optimized in a forward pass, the pass is carried backward: this back-and-forth optimization constitutes a full DMRG sweep. The maximum number of sweeps can be set with `max_sweeps`. Compression is performed at each step, controlled by setting a maximal bond dimension and a cutoff. The `bond_dims` and `cutoffs` arguments take either a single value or a sequence of values (if one wants to assign different parameters to different sweeps).
 #
 # For an introduction on DMRG, see [this presentation](https://tensornetwork.org/mps/algorithms/dmrg/) and references therein.
 
@@ -125,7 +125,7 @@ o2_mpo_original = o2_ham.to_mpo()
 display(o2_mpo_original)
 
 # %% [markdown]
-# To make sure that we don't lose physical information when compressing, we measure the MPO norm
+# To make sure that we don't lose physical information when compressing, we measure the MPO norm.
 
 # %%
 norm = o2_mpo_original.norm()
@@ -148,7 +148,7 @@ print(o2_mpo.norm() / norm)
 print(np.sqrt(o2_mpo.H @ o2_mpo_original) / norm)
 
 # %% [markdown]
-# We now run DMRG
+# We now run DMRG.
 
 # %%
 # %%time
@@ -173,7 +173,7 @@ print(f"{basis} UCCSD energy = {o2_ham.e_ccsd}")
 # %% [markdown]
 # ## $H_2O$
 #
-# We are of course not restricted to molecules with a single atomic element. Descriptions of many molecules — including geometry, spin, charge, and symmetry parameters compatible with $\texttt{pyscf}$ — can be found online. We are only limited by computational power. In the following we take the water molecule in the minimal STO-3G basis.
+# We are of course not restricted to molecules with a single atomic element. Descriptions of many molecules — including geometry, spin, charge, and symmetry parameters compatible with $\texttt{pyscf}$ — can be found online. We are only limited by computational power. In the following, we take the water molecule in the minimal STO-3G basis.
 
 # %%
 basis = "STO-3G"
