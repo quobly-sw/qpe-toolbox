d249d4b40f80cfb6fa614a7b4f735108077cef7f
jwl <jheng-wei.li@quobly.io>
2026-08-11 14:18:16 +0200

    ..

--- tests/test_circuits_opt.py
+++ tests/test_circuits_opt.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 
+import quimb.tensor as qtn
+
 from qpe_toolbox.circuit import ansatz_circuit_su4, tn_fit
 
 
 def test_tn_fit():
     circ = ansatz_circuit_su4(2, 1, param_scaling=1.0, parametrize=False)
     tn1 = circ.psi
-    tn2 = tn1
-    tn_fit(tn1, tn2, tags="SU4", steps=0, tol=1e-10)
+    tn2 = qtn.MPS_rand_state(2, 4)
+    tn_fit(tn1, tn2, tags="SU4", steps=1000, tol=1e-10)
 
     ovlp = abs((tn2.H & tn1).contract())
     assert abs(ovlp - 1.0) < 1e-8
