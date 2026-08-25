d269fd7f8eed0408095b279d2be090938be4c520
Olivier Gauthé <olivier.gauthe@quobly.io>
2026-08-25 11:59:18 +0200

    recover cmaes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,6 @@ templates_path = ["_templates"]
 # -- Link to other documentation -----------------------------------------------
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "networkx": ("https://networkx.org/documentation/stable/", None),
 }
 
 # -- Napoleon configuration ---------------------------------------------------
