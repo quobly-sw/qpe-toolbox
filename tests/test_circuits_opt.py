c06ccc295d3f089de255b31ec8e14f901dcb3014
jwl <jheng-wei.li@quobly.io>
2026-08-27 13:47:25 +0200

    minor

--- tests/test_circuits_opt.py
+++ tests/test_circuits_opt.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-
+import numpy as np
 import quimb.tensor as qtn
 
 from qpe_toolbox.circuit import ansatz_circuit_su4, tn_fit
 
 
 def test_tn_fit():
-    circ = ansatz_circuit_su4(2, 1, param_scaling=1.0, parametrize=False)
+    rng = np.random.default_rng(42)
+
+    circ = ansatz_circuit_su4(2, 1, param_scaling=1.0, parametrize=False, rng=rng)
     tn1 = circ.psi
     tn2 = qtn.MPS_rand_state(2, 4)
     tn_fit(tn1, tn2, tags="SU4", steps=1000, tol=1e-10)
