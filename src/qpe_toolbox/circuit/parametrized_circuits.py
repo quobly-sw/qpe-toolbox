d4a3144cb7610cf3a122a31144c7e98c32e9ab27
Olivier Gauthé <olivier.gauthe@quobly.io>
2026-09-04 15:00:42 +0200

    TODO REVERT check numpydoc CI

--- src/qpe_toolbox/circuit/parametrized_circuits.py
+++ src/qpe_toolbox/circuit/parametrized_circuits.py
@@ -119,7 +119,6 @@ def two_qubit_nn_layer(
     neighbors in a brickwork pattern. The starting qubit index determines
     the parity of the layer.
 
-    Parameters
     ----------
     circ : :quimb-api:`Circuit`
         The ``quimb`` circuit to which the layer is applied.
