d80b06cb02a12698ef72c38fb334a84cfabd1e52
Olivier Gauthé <olivier.gauthe@quobly.io>
2026-09-23 11:43:27 +0200

    shorten imports

--- src/qpe_toolbox/estimation/__init__.py
+++ src/qpe_toolbox/estimation/__init__.py
@@ -21,10 +21,7 @@ from .lcu_walk_operator import (
     run_qpe_lcu_walk_operator,
 )
 from .qft import iqft, iqft_swapped
-from .qpe_circuit import (
-    qpe_circuit,
-    qpe_gates,
-)
+from .qpe_circuit import qpe_circuit, qpe_gates
 from .quantum_phase_estimation import (
     exact_evolution_powers,
     qpe_energy,
